ae130ea72c8ec5f6ea7b3f681dd2c3e91684e0e9
AlexQ-117 <160072929+AlexQ-117@users.noreply.github.com>
2026-01-22 12:16:54 -0800

    Clean up header comments in immgena.sv
    Removed header comments from immgena.sv

--- src/ID/immgena.sv
+++ src/ID/immgena.sv
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/22/2026 12:09:35 PM
-// Design Name: 
-// Module Name: immgena
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module immgena(
