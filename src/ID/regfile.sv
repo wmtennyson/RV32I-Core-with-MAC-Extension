d146ce01c5898f0122d5bc127f7580873077f867
AlexQ-117 <160072929+AlexQ-117@users.noreply.github.com>
2026-01-22 12:16:40 -0800

    Clean up header comments in regfile.sv
    Removed header comments from regfile.sv.

---
+++ src/ID/regfile.sv
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module regfile(
+    input  logic        clk,
+    input  logic        rst,
+
+    input  logic        we_i,
+    input  logic [4:0]  waddr_i,
+    input  logic [31:0] wdata_i,
+
+    input  logic [4:0]  raddr1_i,
+    input  logic [4:0]  raddr2_i,
+    output logic [31:0] rdata1_o,
+    output logic [31:0] rdata2_o
+);
+    logic [31:0] regs [31:0];
+
+    integer k;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            for (k = 0; k < 32; k++) regs[k] <= 32'd0;
+        end else begin
+            if (we_i && (waddr_i != 5'd0)) begin
+                regs[waddr_i] <= wdata_i;
+            end
+        end
+    end
+
+    always_comb begin
+        rdata1_o = (raddr1_i == 5'd0) ? 32'd0 : regs[raddr1_i];
+        rdata2_o = (raddr2_i == 5'd0) ? 32'd0 : regs[raddr2_i];
+    end
+endmodule
