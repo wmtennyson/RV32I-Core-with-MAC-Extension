14edcaa55d663fe3ab0674e06e4d2a998835518b
wmtennyson <w_tennyson@outlook.com>
2026-03-18 21:27:38 -0700

    Refactor EXE_Control for ALU control signals

--- src/EX/EXE_Control.sv
+++ src/EX/EXE_Control.sv
@@ -1,7 +1,7 @@
 `include "Def.vh"
 `timescale 1ns / 1ps
 
-module EXE_Control(
+module EXE_Control (
     input  logic [2:0] alu_op,
     input  logic [6:0] func7,
     input  logic [2:0] func3,
@@ -9,59 +9,46 @@ module EXE_Control(
 );
 
     always_comb begin
-        // Default
-        alu_ctrl = `ADD;
+        alu_ctrl = `ALU_ADD;
 
         unique casez ({alu_op, func7[5], func3})
+            {`OP_STORE,  1'b?, 3'b???},
+            {`OP_UTYPE,  1'b?, 3'b???},
+            {`OP_NOP,    1'b?, 3'b???},
+            {`OP_LOAD,   1'b?, 3'b???},
+            {`OP_JUMP,   1'b?, 3'b???},
+            {`OP_RTYPE,  1'b0, 3'b000},
+            {`OP_ITYPE,  1'b?, 3'b000}: alu_ctrl = `ALU_ADD;
 
-            // ADD-like (address calc, etc.)
-            {`STORE,  1'b?, 3'b???},
-            {`U_TYPE, 1'b?, 3'b???},
-            {`NOP,    1'b?, 3'b???},
-            {`LOAD,   1'b?, 3'b???},
-            {`JUMP,   1'b?, 3'b???},  
-            {`R_TYPE, 1'b0, 3'b000},
-            {`I_TYPE, 1'b?, 3'b000}: alu_ctrl = `ADD;
+            {`OP_RTYPE,  1'b1, 3'b000}: alu_ctrl = `ALU_SUB;
 
-            // SUB
-            {`R_TYPE, 1'b1, 3'b000}: alu_ctrl = `SUB;
+            {`OP_RTYPE,  1'b0, 3'b111},
+            {`OP_ITYPE,  1'b?, 3'b111}: alu_ctrl = `ALU_AND;
 
-            // AND / ANDI
-            {`R_TYPE, 1'b0, 3'b111},
-            {`I_TYPE, 1'b?, 3'b111}: alu_ctrl = `AND;
+            {`OP_RTYPE,  1'b0, 3'b110},
+            {`OP_ITYPE,  1'b?, 3'b110}: alu_ctrl = `ALU_OR;
 
-            // OR / ORI
-            {`R_TYPE, 1'b0, 3'b110},
-            {`I_TYPE, 1'b?, 3'b110}: alu_ctrl = `OR;
+            {`OP_RTYPE,  1'b0, 3'b100},
+            {`OP_ITYPE,  1'b?, 3'b100}: alu_ctrl = `ALU_XOR;
 
-            // XOR / XORI
-            {`R_TYPE, 1'b0, 3'b100},
-            {`I_TYPE, 1'b?, 3'b100}: alu_ctrl = `XOR;
+            {`OP_RTYPE,  1'b0, 3'b001},
+            {`OP_ITYPE,  1'b0, 3'b001}: alu_ctrl = `ALU_SLL;
 
-            // SLL / SLLI
-            {`R_TYPE, 1'b0, 3'b001},
-            {`I_TYPE, 1'b0, 3'b001}: alu_ctrl = `SLL;
+            {`OP_RTYPE,  1'b0, 3'b101},
+            {`OP_ITYPE,  1'b0, 3'b101}: alu_ctrl = `ALU_SRL;
 
-            // SRL / SRLI
-            {`R_TYPE, 1'b0, 3'b101},
-            {`I_TYPE, 1'b0, 3'b101}: alu_ctrl = `SRL;
+            {`OP_RTYPE,  1'b1, 3'b101},
+            {`OP_ITYPE,  1'b1, 3'b101}: alu_ctrl = `ALU_SRA;
 
-            // SRA / SRAI
-            {`R_TYPE, 1'b1, 3'b101},
-            {`I_TYPE, 1'b1, 3'b101}: alu_ctrl = `SRA;
+            {`OP_RTYPE,  1'b0, 3'b010},
+            {`OP_ITYPE,  1'b?, 3'b010}: alu_ctrl = `ALU_SLT;
 
-            // SLT / SLTI
-            {`R_TYPE, 1'b0, 3'b010},
-            {`I_TYPE, 1'b?, 3'b010}: alu_ctrl = `SLT;
+            {`OP_RTYPE,  1'b0, 3'b011},
+            {`OP_ITYPE,  1'b?, 3'b011}: alu_ctrl = `ALU_SLTU;
 
-            // SLTU / SLTIU
-            {`R_TYPE, 1'b0, 3'b011},
-            {`I_TYPE, 1'b?, 3'b011}: alu_ctrl = `SLTU;
+            {`OP_BRANCH, 1'b?, 3'b???}: alu_ctrl = `ALU_SUB;
 
-            // Branch Types (Redundancy)
-            {`BRANCH, 1'b?, 3'b???}: alu_ctrl = `SUB;
-
-            default: ; // keep default `ADD
+            default: ;
         endcase
     end
 
