a7dc86a8c2437414437d7d317f5acac6364d3624
wmtennyson <w_tennyson@outlook.com>
2026-03-18 21:28:19 -0700

    Update EXE_ALU.sv

--- src/EX/EXE_ALU.sv
+++ src/EX/EXE_ALU.sv
@@ -1,39 +1,30 @@
 `include "Def.vh"
 `timescale 1ns / 1ps
 
-module EXE_ALU(
-        // Inputs
-        input logic [31:0] OpA,
-                           OpB,
-        input logic [3:0]  alu_ctrl,
-              
-        // Outputs-Variable
-        output logic [31:0] alu_out
-    );    
-       
-    // Get Shift Amount
+module EXE_ALU (
+    input  logic [31:0] OpA,
+    input  logic [31:0] OpB,
+    input  logic [3:0]  alu_ctrl,
+    output logic [31:0] alu_out
+);
+
     logic [4:0] shamt;
-    assign shamt = OpB[4:0];   
-        
-    // Begin Switch Statement
+    assign shamt = OpB[4:0];
+
     always_comb begin
-    
-        // Default
-        alu_out = '0;  
-    
-        unique case(alu_ctrl)
-            `ADD  : alu_out = OpA + OpB;                                            // Addtion
-            `SUB  : alu_out = OpA - OpB;                                            // Subtraction
-            `AND  : alu_out = OpA & OpB;                                            // AND
-            `OR   : alu_out = OpA | OpB;                                            // OR
-            `XOR  : alu_out = OpA ^ OpB;                                            // XOR
-            `SLL  : alu_out = OpA << shamt;                                         // Shift Left Logical
-            `SRL  : alu_out = OpA >> shamt;                                         // Shift Right Logical
-            `SRA  : alu_out = $signed(OpA) >>> shamt;                               // Arithmetic Right Shift, Signed 
-            `SLT  : alu_out = {31'b0, ($signed(OpA) < $signed(OpB))};               // Set Signed Less-Than
-            `SLTU : alu_out = {31'b0, OpA < OpB};                                   // Set Unsinged Less-Than
-             default : ;                                                            // Error
-                
+        alu_out = 32'd0;
+        unique case (alu_ctrl)
+            `ALU_ADD  : alu_out = OpA + OpB;
+            `ALU_SUB  : alu_out = OpA - OpB;
+            `ALU_AND  : alu_out = OpA & OpB;
+            `ALU_OR   : alu_out = OpA | OpB;
+            `ALU_XOR  : alu_out = OpA ^ OpB;
+            `ALU_SLL  : alu_out = OpA << shamt;
+            `ALU_SRL  : alu_out = OpA >> shamt;
+            `ALU_SRA  : alu_out = $signed(OpA) >>> shamt;
+            `ALU_SLT  : alu_out = {31'b0, ($signed(OpA) < $signed(OpB))};
+            `ALU_SLTU : alu_out = {31'b0, (OpA < OpB)};
+            default   : ;
         endcase
     end
 
