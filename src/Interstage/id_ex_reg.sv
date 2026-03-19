ed351cc49f6debdabb5f8e71f2b573036962de3b
wmtennyson <w_tennyson@outlook.com>
2026-03-18 21:23:43 -0700

    Refactor id_ex_reg module inputs and outputs

--- src/Interstage/id_ex_reg.sv
+++ src/Interstage/id_ex_reg.sv
@@ -1,154 +1,113 @@
-`timescale 1ns/1ps
 `include "Def.vh"
+`timescale 1ns / 1ps
 
 module id_ex_reg (
-    input  logic        clk,
-    input  logic        rst,
+    input  logic            clk,
+    input  logic            rst,
+    input  logic            flush_i,
+    input  logic            stall_i,
 
-    input  logic        flush_i,
-    input  logic        stall_i,
-    input  logic        instr_valid_i,
-    input  logic [31:0] instr_i,
+    // From Decode
+    input  logic            valid_i,
+    input  logic [31:0]     instr_i,
+    input  logic [31:0]     pc_i,
+    input  logic [31:0]     pc4_i,
+    input  logic [31:0]     rs1_val_i,
+    input  logic [31:0]     rs2_val_i,
+    input  logic [31:0]     imm_i,
+    input  logic [4:0]      rs1_i, 
+                            rs2_i, 
+                            rd_i,
+    input  logic [2:0]      funct3_i,
+    input  logic [6:0]      funct7_i,
+    input  logic            regwrite_i, 
+                            mem_read_i, 
+                            mem_write_i,
+                            branch_i, 
+                            jump_i, 
+                            is_jalr_i, 
+                            lui_i,
+                            opA_sel_i, 
+                            opB_sel_i,
+                            wb_sel_i, 
+                            wb_pc4_sel_i,
+    input  logic [2:0]      alu_op_i,
 
-    // ID-stage data inputs
-    input  logic [31:0] pc_i,
-    input  logic [31:0] pc4_i,
-    input  logic [31:0] rs1_val_i,
-    input  logic [31:0] rs2_val_i,
-    input  logic [31:0] imm_i,
-
-    input  logic [4:0]  rs1_i,
-    input  logic [4:0]  rs2_i,
-    input  logic [4:0]  rd_i,
-    input  logic [2:0]  funct3_i,
-    input  logic [6:0]  funct7_i,
-
-    // ID-stage control inputs (already gated in decode if you did that)
-    input  logic        regwrite_i,
-    input  logic        mem_read_i,
-    input  logic        mem_write_i,
-    input  logic        branch_i,
-    input  logic        jump_i,
-    input  logic        write_data_i,
-    input  logic        wb_pc4_sel_i,
-    input  logic        lui_i,
-    input  logic        is_jalr_i,
-    input  logic [2:0]  alu_op_i,
-
-    // Execute select inputs
-    input  logic        opA_sel_i,     
-    input  logic        opB_sel_i,         
-
-    // ID/EX registered outputs
-    output logic        id_ex_valid_o,
-    output logic [31:0] id_ex_instr_o,
-
-    output logic [31:0] id_ex_pc_o,
-    output logic [31:0] id_ex_pc4_o,
-    output logic [31:0] id_ex_rs1_val_o,
-    output logic [31:0] id_ex_rs2_val_o,
-    output logic [31:0] id_ex_imm_o,
-
-    output logic [4:0]  id_ex_rs1_o,
-    output logic [4:0]  id_ex_rs2_o,
-    output logic [4:0]  id_ex_rd_o,
-    output logic [2:0]  id_ex_funct3_o,
-    output logic [6:0]  id_ex_funct7_o,
-
-    output logic        id_ex_regwrite_o,
-    output logic        id_ex_mem_read_o,
-    output logic        id_ex_mem_write_o,
-    output logic        id_ex_branch_o,
-    output logic        id_ex_jump_o,
-    output logic        id_ex_write_data_o,
-    output logic        id_ex_wb_pc4_sel_o,
-
-    output logic        id_ex_lui_o,
-    output logic        id_ex_is_jalr_o,
-    output logic [2:0]  id_ex_alu_op_o,
-
-    output logic        id_ex_opA_sel_o,  
-    output logic        id_ex_opB_sel_o
+    // Registered outputs
+    output logic        valid_o,
+    output logic [31:0] instr_o,
+    output logic [31:0] pc_o, pc4_o,
+    output logic [31:0] rs1_val_o, rs2_val_o, imm_o,
+    output logic [4:0]  rs1_o, rs2_o, rd_o,
+    output logic [2:0]  funct3_o,
+    output logic [6:0]  funct7_o,
+    output logic        regwrite_o, 
+                        mem_read_o, 
+                        mem_write_o,
+                        branch_o, 
+                        jump_o, 
+                        is_jalr_o, 
+                        lui_o,
+                        opA_sel_o, 
+                        opB_sel_o,
+                        wb_sel_o, 
+                        wb_pc4_sel_o,
+    output logic [2:0]  alu_op_o
 );
 
-    task automatic set_idex_nop();
-        begin
-            id_ex_valid_o       <= 1'b0;
-            id_ex_instr_o       <= 32'h00000013;  // NOP (ADDI x0,x0,0)
-
-            id_ex_pc_o          <= 32'd0;
-            id_ex_pc4_o         <= 32'd0;
-            id_ex_rs1_val_o     <= 32'd0;
-            id_ex_rs2_val_o     <= 32'd0;
-            id_ex_imm_o         <= 32'd0;
-
-            id_ex_rs1_o         <= 5'd0;
-            id_ex_rs2_o         <= 5'd0;
-            id_ex_rd_o          <= 5'd0;
-            id_ex_funct3_o      <= 3'd0;
-            id_ex_funct7_o      <= 7'd0;
-
-            id_ex_regwrite_o    <= 1'b0;
-            id_ex_mem_read_o    <= 1'b0;
-            id_ex_mem_write_o   <= 1'b0;
-            id_ex_branch_o      <= 1'b0;
-            id_ex_jump_o        <= 1'b0;
-            id_ex_write_data_o  <= 1'b0;
-            id_ex_wb_pc4_sel_o  <= 1'b0;
-            id_ex_lui_o         <= 1'b0;
-            id_ex_is_jalr_o     <= 1'b0;
-            id_ex_alu_op_o      <= `NOP;
-
-            id_ex_opA_sel_o     <= 1'b0; 
-            id_ex_opB_sel_o     <= 1'b0;
-
-        end
-    endtask
-
-    // -------------------------
-    // ID/EX pipeline register
-    // -------------------------
     always_ff @(posedge clk) begin
-        if (rst) begin
-            set_idex_nop();
-        end else if (flush_i) begin
-            set_idex_nop();
-        end else if (stall_i) begin
-            // Hold State
-        end else if (!instr_valid_i) begin
-            set_idex_nop();
-        end else begin
-            id_ex_valid_o       <= 1'b1;
-            id_ex_instr_o       <= instr_i;
-        
-            id_ex_pc_o          <= pc_i;
-            id_ex_pc4_o         <= pc4_i;
-            id_ex_rs1_val_o     <= rs1_val_i;
-            id_ex_rs2_val_o     <= rs2_val_i;
-            id_ex_imm_o         <= imm_i;
-        
-            id_ex_rs1_o         <= rs1_i;
-            id_ex_rs2_o         <= rs2_i;
-            id_ex_rd_o          <= rd_i;
-            id_ex_funct3_o      <= funct3_i;
-            id_ex_funct7_o      <= funct7_i;
-        
-            id_ex_regwrite_o    <= regwrite_i;
-            id_ex_mem_read_o    <= mem_read_i;
-            id_ex_mem_write_o   <= mem_write_i;
-            id_ex_branch_o      <= branch_i;
-            id_ex_jump_o        <= jump_i;
-            id_ex_write_data_o  <= write_data_i;
-            id_ex_wb_pc4_sel_o  <= wb_pc4_sel_i;
-            id_ex_lui_o         <= lui_i;
-            id_ex_is_jalr_o     <= is_jalr_i;
-            id_ex_alu_op_o      <= alu_op_i;
-        
-            id_ex_opA_sel_o     <= opA_sel_i;
-            id_ex_opB_sel_o     <= opB_sel_i;
-        
-        end 
+        if (rst || flush_i) begin
+            valid_o         <= 1'b0;   
+            instr_o         <= 32'h0000_0013;
+            pc_o            <= 32'd0;  
+            pc4_o           <= 32'd0;
+            rs1_val_o       <= 32'd0;  
+            rs2_val_o       <= 32'd0;  
+            imm_o           <= 32'd0;
+            rs1_o           <= 5'd0;   
+            rs2_o           <= 5'd0;    
+            rd_o            <= 5'd0;
+            funct3_o        <= 3'd0;   
+            funct7_o        <= 7'd0;
+            regwrite_o      <= 1'b0;   
+            mem_read_o      <= 1'b0;  
+            mem_write_o     <= 1'b0;
+            branch_o        <= 1'b0;   
+            jump_o          <= 1'b0;    
+            is_jalr_o       <= 1'b0;
+            lui_o           <= 1'b0;   
+            opA_sel_o       <= 1'b0;    
+            opB_sel_o       <= 1'b0;
+            wb_sel_o        <= 1'b0;   
+            wb_pc4_sel_o    <= 1'b0;
+            alu_op_o        <= `OP_NOP;
+        end
+        else if (!stall_i) begin
+            valid_o         <= valid_i;     
+            instr_o         <= instr_i;
+            pc_o            <= pc_i;        
+            pc4_o           <= pc4_i;
+            rs1_val_o       <= rs1_val_i;   
+            rs2_val_o       <= rs2_val_i;  
+            imm_o           <= imm_i;
+            rs1_o           <= rs1_i;       
+            rs2_o           <= rs2_i;       
+            rd_o            <= rd_i;
+            funct3_o        <= funct3_i;    
+            funct7_o        <= funct7_i;
+            regwrite_o      <= regwrite_i & valid_i;
+            mem_read_o      <= mem_read_i  & valid_i;
+            mem_write_o     <= mem_write_i & valid_i;
+            branch_o        <= branch_i    & valid_i;
+            jump_o          <= jump_i      & valid_i;
+            is_jalr_o       <= is_jalr_i   & valid_i;
+            lui_o           <= lui_i       & valid_i;
+            opA_sel_o       <= opA_sel_i;   
+            opB_sel_o       <= opB_sel_i;
+            wb_sel_o        <= wb_sel_i    & valid_i;
+            wb_pc4_sel_o    <= wb_pc4_sel_i & valid_i;
+            alu_op_o        <= valid_i ? alu_op_i : `OP_NOP;
+        end
     end
 
 endmodule
-
