317943ebea21ad75ca0d33868c8d389d5eac062c
wmtennyson <w_tennyson@outlook.com>
2026-03-18 21:11:38 -0700

    Refactor ImmGen for improved readability

--- src/ID/ImmGen.sv
+++ src/ID/ImmGen.sv
@@ -1,41 +1,34 @@
 `timescale 1ns / 1ps
 
-module ImmGen(
+module ImmGen (
     input  logic [31:0] instr_i,
     output logic [31:0] imm_o
 );
+
     logic [6:0] opcode;
     assign opcode = instr_i[6:0];
 
     always_comb begin
         unique case (opcode)
-            // I-type (ADDI, ANDI, ORI, XORI, SLTI, loads, JALR)
-            7'b0010011, 7'b0000011, 7'b1100111: begin
+            7'b0010011, 7'b0000011, 7'b1100111:                // I-type, Load, JALR
                 imm_o = {{20{instr_i[31]}}, instr_i[31:20]};
-            end
 
-            // S-type (stores)
-            7'b0100011: begin
+            7'b0100011:                                         // S-type
                 imm_o = {{20{instr_i[31]}}, instr_i[31:25], instr_i[11:7]};
-            end
 
-            // B-type (branches)
-            7'b1100011: begin
-                imm_o = {{19{instr_i[31]}}, instr_i[31], instr_i[7], instr_i[30:25], instr_i[11:8], 1'b0};
-            end
+            7'b1100011:                                         // B-type
+                imm_o = {{19{instr_i[31]}}, instr_i[31], instr_i[7],
+                          instr_i[30:25], instr_i[11:8], 1'b0};
 
-            // U-type (LUI/AUIPC)
-            7'b0110111, 7'b0010111: begin
+            7'b0110111, 7'b0010111:                             // U-type
                 imm_o = {instr_i[31:12], 12'b0};
-            end
 
-            // J-type (JAL)
-            7'b1101111: begin
-                imm_o = {{11{instr_i[31]}}, instr_i[31], instr_i[19:12], instr_i[20], instr_i[30:21], 1'b0};
-            end
+            7'b1101111:                                         // J-type
+                imm_o = {{11{instr_i[31]}}, instr_i[31], instr_i[19:12],
+                          instr_i[20], instr_i[30:21], 1'b0};
 
             default: imm_o = 32'd0;
         endcase
     end
-endmodule
 
+endmodule
